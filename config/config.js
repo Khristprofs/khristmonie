f1af1153232c692b0f92f662bb58f0c0478d3b06
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2026-05-22 15:50:27 +0100

    Made changes

--- config/config.js
+++ config/config.js
@@ -3,11 +3,31 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'Prof.khrist@0602',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'khristmonie',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
+    logging: false
   },
-  // Add test and production environments as needed
-  logging: false // Disables SQL query logging
+
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'khristmonie_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres',
+    logging: false
+  },
+
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  }
 };
