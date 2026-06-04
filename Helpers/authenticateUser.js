37e278ef13bd853c1c814628593c00f0ef5ac3fe
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2026-06-04 18:25:16 +0100

    Made changes

--- Helpers/authenticateUser.js
+++ Helpers/authenticateUser.js
@@ -1,51 +1,43 @@
-const { User } = require('../models');
+const { User, sequelize } = require('../models');
 const bcrypt = require('bcryptjs');
-const db = require('../models');
 
 console.log(Object.keys(db));
 console.log(db.User);
 
 const authenticateUser = async (email, password) => {
-    console.log('User table:', User.getTableName());
-    console.log("Searching email:", email);
-    console.log("Table:", User.getTableName());
+    console.log("================================");
+    console.log("Searching for:", email);
+    console.log("Table name:", User.getTableName());
+
+    const count = await User.count();
+    console.log("Total users:", count);
+
+    const [tables] = await sequelize.query(`
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+    `);
+
+    console.log("Tables:", tables);
+
+    const users = await User.findAll({
+        attributes: ['id', 'email'],
+        limit: 10
+    });
+
+    console.log("Users found:", users);
+
     const user = await User.findOne({
         where: { email }
     });
-    console.log("Found:", user);
+
+    console.log("Matched user:", user);
 
     if (!user) {
-        throw new Error('User not found');
+        throw new Error("User not found");
     }
 
-    const isMatch = await bcrypt.compare(
-        password,
-        user.password
-    );
-
-    if (!isMatch) {
-        throw new Error('Invalid credentials');
-    }
-    console.log("Table Name:", User.getTableName());
-
-    const tables = await sequelize.query(
-        `SELECT table_name
-   FROM information_schema.tables
-   WHERE table_schema = 'public'`
-    );
-
-    console.log("Tables:", tables[0]);
-
-    return {
-        _id: user.id,
-        firstname: user.firstname,
-        middlename: user.middlename,
-        lastname: user.lastname,
-        email: user.email,
-        phone: user.phone,
-        nin: user.nin,
-        role: user.role,
-    };
+    return user;
 };
 
 module.exports = authenticateUser;
